2017faebcbe0060fb558d876cf421eeda1e54188
Rob Dunne <rob.dunne@gmail.com>
2020-06-02 13:02:22 +1000

    small changes

---
+++ R/dpsschk.R
@@ -0,0 +1,18 @@
+#'  function to calculate tapers
+#'
+#' Called by spectral estimation routines to generate the tapers
+#'
+#' @param tapers  NW K - time-bandwidth product, number of tapers
+#' @param N (number of samples
+#' @param Fs sampling frequency
+#' @return tapers   
+#' 
+#' @export
+dpsschk <-function(tapers,N,Fs){
+    sz<- dim(tapers)
+    if (sz[1]==1 && sz[2]==2){
+        tapers <-multitaper::dpss(N,tapers[2],tapers[1])
+        tapers$v = tapers$v*sqrt(Fs)
+    }
+    tapers$v
+}
